03913afbc6133a579cc305132b8dd6cfb896f5f9
wim glenn <wim.glenn@gmail.com>
2018-01-18 16:31:45 -0600

    grouper and ceiling division (#3)
    grouper and ceiling division

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='wimpy',
-    version='0.2',
+    version='0.3',
     description='Anti-copy-pasta',
     url='https://github.com/wimglenn/wimpy',
     author='Wim Glenn',
