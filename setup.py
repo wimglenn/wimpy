4e8ebe4e7052d88c9f88ac7dcaa1b587cc2cf86e
wim glenn <hey@wimglenn.com>
2018-10-09 00:59:27 -0500

    version bump for release

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from setuptools import setup
 
 setup(
     name="wimpy",
-    version="0.5",
+    version="0.6",
     description="Anti-copy-pasta",
+    long_description=open('README.rst').read(),
     url="https://github.com/wimglenn/wimpy",
     author="Wim Glenn",
     author_email="hey@wimglenn.com",
